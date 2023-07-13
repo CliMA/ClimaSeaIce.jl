f503aa96a5673c62b517aa34e00c73a871ad4c42
Gregory Wagner <wagner.greg@gmail.com>
2023-07-13 13:21:27 -0600

    Start building a coupled ice-ocean column model setup

--- examples/ice_ocean_column_model.jl
+++ examples/ice_ocean_column_model.jl
@@ -1,7 +1,8 @@
-using ClimaSeaIce: ThermodynamicSeaIceModel, MolecularDiffusivity
+using ClimaSeaIce: ThermodynamicSeaIceModel, MolecularDiffusivity, temperature_flux
 using Oceananigans
 using Oceananigans.Units
 using Oceananigans.Operators: Δzᶜᶜᶠ
+using Oceananigans.TurbulenceClosures: CATKEVerticalDiffusivity
 using Oceananigans.Grids: znode
 using GLMakie
 
@@ -20,8 +21,8 @@ S_bcs = FieldBoundaryConditions(top=FluxBoundaryCondition(top_S_flux))
 
 ocean_model = HydrostaticFreeSurfaceModel(grid = ocean_grid,
                                           closure = CATKEVerticalDiffusivity(),
-                                          tracers = (:T, :S),
-                                          buoyancy = SeawaterBuoyancy(; equation_of_state)
+                                          tracers = (:T, :S, :e),
+                                          buoyancy = SeawaterBuoyancy(; equation_of_state),
                                           boundary_conditions = (; T=T_bcs, S=S_bcs))
 
 
@@ -40,136 +41,72 @@ set!(ocean_model, T=Tᵢ, S=Sᵢ)
 #####
 
 # Build a grid with 10 cm resolution
-grid = RectilinearGrid(size=20, z=(-1, 0), topology=(Flat, Flat, Bounded))
+ice_grid = RectilinearGrid(size=20, z=(-1, 0), topology=(Flat, Flat, Bounded))
 
 # Set up a simple problem and build the ice model
-atmosphere_temperature = -10  # ᵒC
-ocean_temperature      = 0.1  # ᵒC
-closure = MolecularDiffusivity(grid, κ_ice=1e-5, κ_water=1e-6)
-model = ThermodynamicSeaIceModel(; grid, closure, atmosphere_temperature, ocean_temperature)
+ice_closure = MolecularDiffusivity(ice_grid, κ_ice=1e-5, κ_water=1e-6)
 
-# Initialize and run
-set!(model, T=ocean_temperature)
+#####
+##### Create temperature boundary conditions
+#####
 
-H = model.state.H
-@show H
+# Atmopshere-ice surface temperature parameters
+initial_air_ice_temperature = -5
+air_ice_temperature_fluctuation_amplitude = 5
+air_ice_tendency = -0.5 / day # ᵒC s⁻¹
 
-# We're using explicit time stepping. The CFL condition is
-#
-#   Δt < Δz² / κ ≈ 0.1² / 1e-6 ≈ 1e4,
+parameters = (Tᵢ = initial_air_ice_temperature,
+              Tₐ = air_ice_temperature_fluctuation_amplitude,
+              dTdt = air_ice_tendency)
+
+@inline air_ice_temperature(x, y, t, p) = p.dTdt * t + p.Tₐ * sin(2π*t / day) + p.Tᵢ
+top_T_bc = ValueBoundaryCondition(air_ice_temperature; parameters)
+T_bcs = FieldBoundaryConditions(top=top_T_bc)
+
+ice_model = ThermodynamicSeaIceModel(grid = ice_grid,
+                                     closure = ice_closure,
+                                     boundary_conditions=(; T=T_bcs))
+
 
 κ = 1e-5
-Δz = Δzᶜᶜᶠ(1, 1, 1, grid)
+Δz = Δzᶜᶜᶠ(1, 1, 1, ice_grid)
 Δt = 0.1 * Δz^2 / κ
-simulation = Simulation(model; Δt)
 
-@show simulation
+ice_simulation = Simulation(ice_model; Δt, stop_iteration=10)
 
-#####
-##### Set up diagnostics
-#####
+@inline is_ocean(i, j, k, grid, ϕ) = @inbounds ϕ[i, j, k] == 1
 
-const c = Center()
-
-ht = Float64[]
-th = Float64[]
-
-function compute_ice_thickness(sim; melting_temperature=-0.1)
+function fix_liquid_temperature!(ϕ_ice_model, T_ice_model, grid, T_ocean_model) 
     Nz = size(grid, 3)
-    T = sim.model.state.T # model temperature
-    Ti = interior(T, 1, 1, :)
-    kw = searchsortedlast(Ti, melting_temperature, rev=true)
-    ki = kw + 1 # ice index
 
-    # Linearly interpolate to obtain thickness
-    Δz = Δzᶜᶜᶠ(1, 1, ki, grid)
-    zw = znode(1, 1, kw, grid, c, c, c)
-    dTdz = (T[1, 1, ki] - T[1, 1, kw]) / Δz
-    z = zw + (melting_temperature - T[1, 1, kw]) / dTdz
-    h = -z # thickness
-
-    # Store result
-    push!(ht, h)
-    push!(th, time(sim))
+    i = j = 1
+    @inbounds for k = 1:Nz
+        T_ice_model[i, j, k] = ifelse(is_ocean(i, j, k, grid, ϕ_ice_model),
+                                      T_ocean_model[i, j],
+                                      T_ice_model[i, j, k])
+    end
 
     return nothing
 end
 
-simulation.callbacks[:thickness] = Callback(compute_ice_thickness, IterationInterval(1))
+function set_liquid_state!(ice_simulation)
+    ice_model = ice_simulation.model
 
-tt = [] 
-Tt = [] 
-Ht = []
-ϕt = []
-κt = []
+    # Extract ocean temperature at top grid point
+    ocean_Nz = size(ocean_model.grid, 3)
+    ocean_temperature = interior(ocean_model.tracers.T, :, :, ocean_Nz)
 
-function grab_profiles!(sim)
-    # Fields of interest
-    T = sim.model.state.T
-    H = sim.model.state.H
-    ϕ = sim.model.state.ϕ
-    κ = sim.model.closure.κ
+    # Eventually this will launch a kernel on the GPU
+    ϕ = ice_model.state.ϕ
+    T = ice_model.state.T
+    fix_liquid_temperature!(ϕ, T, ice_model.grid, ocean_temperature)
 
-    # Extract interior data (excluding halos)
-    Ti = interior(T, 1, 1, :)  
-    Hi = interior(H, 1, 1, :)
-    ϕi = interior(ϕ, 1, 1, :)
-    κi = interior(κ, 1, 1, :)
-
-    # Copy data into time-series vectors
-    push!(tt, time(sim))
-    push!(Tt, deepcopy(Ti))
-    push!(Ht, deepcopy(Hi))
-    push!(ϕt, deepcopy(ϕi))
-    push!(κt, deepcopy(κi))
 
     return nothing
 end
 
-simulation.callbacks[:grabber] = Callback(grab_profiles!, SpecifiedTimes(10minutes, 30minutes, 1hour))
-
-#####
-##### Run the simulation
-#####
-
-simulation.stop_time = 1hour
-run!(simulation)
-
-# Make a plot
-
-fig = Figure()
-
-axT = Axis(fig[1, 1], xlabel="Temperature (ᵒC)", ylabel="z (m)")
-axH = Axis(fig[1, 2], xlabel="Enthalpy (J m⁻³)", ylabel="z (m)")
-axϕ = Axis(fig[1, 3], xlabel="Porosity", ylabel="z (m)")
-axκ = Axis(fig[1, 4], xlabel="Diffusivity", ylabel="z (m)")
-axh = Axis(fig[2, 1:4], xlabel="Time (hours)", ylabel="Ice thickness (m)")
-
-z = znodes(model.state.T)
+ice_simulation.callbacks[:ocean_state] = Callback(set_liquid_state!)
 
 #=
-# TODO: calculate analytical solution
-ℒ = model.fusion_enthalpy
-ΔT = ocean_temperature - atmosphere_temperature 
-c = ice_heat_capacity 
-f(λ) = λ * exp(λ^2) * erf(λ) - St / sqrt(π)
+ocean_simulation = Simulation(ocean_model; Δt)
 =#
-
-for n = 1:length(tt)
-    tn = tt[n]
-    Tn = Tt[n]
-    Hn = Ht[n]
-    ϕn = ϕt[n]
-    κn = κt[n]
-    label = "t = " * prettytime(tn)
-    scatterlines!(axT, Tn, z; label)
-    scatterlines!(axH, Hn, z; label)
-    scatterlines!(axϕ, ϕn, z; label)
-    scatterlines!(axκ, κn, z; label)
-end
-
-lines!(axh, th ./ hour, ht)
-
-axislegend(axT, position=:lb)
-display(fig)
-
