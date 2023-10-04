7986b0e8b56214314f2583438991e6df69ea97b7
Gregory Wagner <wagner.greg@gmail.com>
2023-10-04 13:11:57 -0400

    Change back to CairoMakie

--- examples/freezing_bucket.jl
+++ examples/freezing_bucket.jl
@@ -72,7 +72,7 @@ run!(simulation)
 # It'd be a shame to run such a "cool" simulation without looking at the
 # results. We'll visualize it with Makie.
 
-using GLMakie #CairoMakie
+using CairoMakie
 
 # `timeseries` is a `Vector` of `Tuple`. So we have to do a bit of processing
 # to build `Vector`s of time `t` and thickness `h`. It's not much work though:
