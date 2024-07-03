be0af1ba212d0120ee21c84efcf25c7a52302cb1
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-07-03 13:52:26 -0400

    fix typo

--- src/SlabSeaIceModels/slab_sea_ice_model.jl
+++ src/SlabSeaIceModels/slab_sea_ice_model.jl
@@ -99,7 +99,7 @@ prognostic_fields(model::SSIM) = fields(model)
 Pretty simple model for sea ice.
 """
 function SlabSeaIceModel(grid;
-                         clock                          = Clock{eltype(grid)}(0, 0, 1),
+                         clock                          = Clock{eltype(grid)}(time = 0),
                          ice_thickness                  = Field{Center, Center, Nothing}(grid),
                          ice_consolidation_thickness    = 0.0, # m
                          concentration                  = Field{Center, Center, Nothing}(grid),
