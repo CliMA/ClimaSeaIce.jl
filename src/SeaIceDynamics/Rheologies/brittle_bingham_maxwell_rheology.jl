72f159f5ddd3b0155581aba986fd91659e98e814
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-07-12 16:41:08 +0200

    starting with the BBM rheology

--- src/SeaIceDynamics/Rheologies/brittle_bingham_maxwell_rheology.jl
+++ src/SeaIceDynamics/Rheologies/brittle_bingham_maxwell_rheology.jl
@@ -1,27 +1,41 @@
 
-struct BrittleBinghamMaxwellRheology{S1, S2, S3, D, P, FT} <: AbstractRheology
-    σ₁₁   :: S1 # internal stress xx
-    σ₂₂   :: S2 # internal stress yy
-    σ₁₂   :: S3 # internal stress xy
+struct BrittleBinghamMaxwellRheology{S1, S2, S3, E, η, D, P, FT} <: AbstractRheology
+    σ₁₁ :: S1 # internal stress xx
+    σ₂₂ :: S2 # internal stress yy
+    σ₁₂ :: S3 # internal stress xy
+    young_modulus :: E  # Young modulus
+    bulk_viscosity :: H  # Bulk viscosity
     damage :: D # field containing ice damage
     ice_strength :: P # field containing the precomputed ice strength
     ice_compressive_strength :: FT # compressive strength
     ice_compaction_hardening :: FT # compaction hardening
+    undamaged_young_modulus  :: FT
+    undamaged_bulk_viscosity :: FT
+    bulk_viscosity_exponent :: FT
 end
 
 function BrittleBinghamMaxwellRheology(grid::AbstractGrid; 
                                        ice_compressive_strength = 27500, 
-                                       ice_compaction_hardening = 20)
+                                       ice_compaction_hardening = 20,
+                                       undamaged_young_modulus = 100,
+                                       undamaged_bulk_viscosity = 1e5,
+                                       bulk_viscosity_exponent = 0.1)
     σ₁₁ = CenterField(grid)
     σ₂₂ = CenterField(grid)
     σ₁₂ = Field{Face, Face, Center}(grid)
     
     D  = CenterField(grid)
+    E  = CenterField(grid)
+    H  = CenterField(grid)
     P  = CenterField(grid)
+
     FT = eltype(grid)
-    return BrittleBinghamMaxwellRheology(σ₁₁, σ₂₂, σ₁₂, D, P, 
+    return BrittleBinghamMaxwellRheology(σ₁₁, σ₂₂, σ₁₂, E, H, D, P, 
                                          convert(FT, ice_compressive_strength), 
-                                         convert(FT, ice_compaction_hardening))
+                                         convert(FT, ice_compaction_hardening),
+                                         convert(FT, undamaged_young_modulus), 
+                                         convert(FT, undamaged_bulk_viscosity), 
+                                         convert(FT, bulk_viscosity_exponent))
 end
 
 # Extend the `adapt_structure` function for the BrittleBinghamMaxwellRheology
@@ -29,10 +43,57 @@ Adapt.adapt_structure(to, r::BrittleBinghamMaxwellRheology) =
     BrittleBinghamMaxwellRheology(Adapt.adapt(to, r.σ₁₁),
                                   Adapt.adapt(to, r.σ₂₂),
                                   Adapt.adapt(to, r.σ₁₂),
+                                  Adapt.adapt(to, r.young_modulus),
+                                  Adapt.adapt(to, r.bulk_viscosity),
                                   Adapt.adapt(to, r.damage),
                                   Adapt.adapt(to, r.ice_strength),
                                   Adapt.adapt(to, r.ice_compressive_strength),
-                                  Adapt.adapt(to, r.ice_compaction_hardening))
+                                  Adapt.adapt(to, r.ice_compaction_hardening),
+                                  Adapt.adapt(to, r.undamaged_young_modulus),
+                                  Adapt.adapt(to, r.undamaged_bulk_viscosity),
+                                  Adapt.adapt(to, r.bulk_viscosity_exponent))
+
+
+
+"""
+    initialize_rheology!(model, rheology::BrittleBinghamMaxwellRheology)
+
+Initialize the elasto-visco-plastic rheology.
+In this step we calculate the ice strength given the ice mass (thickness and concentration).
+"""
+function initialize_rheology!(model, rheology::BrittleBinghamMaxwellRheology)
+    h = model.ice_thickness
+    ℵ = model.ice_concentration
+
+    P  = rheology.ice_strength
+    E  = rheology.young_modulus
+    η  = rheology.bulk_viscosity
+
+    P★ = rheology.ice_compressive_strength
+    C  = rheology.ice_compaction_hardening
+
+    E₀ = rheology.undamaged_young_modulus
+    η₀ = rheology.undamaged_bulk_viscosity
+    α  = rheology.bulk_viscosity_exponent
+    
+    launch!(architecture(model.grid), model.grid, :xy, 
+            _compute_initial_parameters!, P, E, η, P★, C, E₀, η₀, α, h, ℵ)
+
+    fill_halo_regions!(P, model.clock, fields(model))
+    
+    return nothing
+end
+
+# The parameterization for an `ExplicitViscoPlasticRheology`
+@kernel function _compute_initial_parameters!(P, E, η, P★, C, E₀, η₀, α, h, ℵ)
+    i, j = @index(Global, NTuple)
+    K₁ = exp(- C * (1 - ℵ[i, j, 1]))
+    K₂ = exp(- α * C * (1 - ℵ[i, j, 1]))
+
+    @inbounds P[i, j, 1] = P★ * h[i, j, 1] * K₁
+    @inbounds E[i, j, 1] = E₀ * K₁ 
+    @inbounds η[i, j, 1] = η₀ * K₂
+end
 
 # Specific compute stresses for the BBM rheology
 function compute_stresses!(model, solver, rheology::BrittleBinghamMaxwellRheology, Δt) 
@@ -56,5 +117,19 @@ end
 @kernel function _compute_bbm_stresses!(rheology, grid, u, v, h, ℵ, ρᵢ, Δt, substeps, stepping_coefficient)
     i, j = @index(Global, NTuple)
 
-    
+    d   = rheology.damage
+    P   = rheology.ice_strength
+    E   = rheology.young_modulus
+    η   = rheology.bulk_viscosity
+    σ₁₁ = rheology.σ₁₁
+    σ₂₂ = rheology.σ₁₁
+    σ₁₂ = rheology.σ₁₁
+
+    @inbuonds E₀ = E[i, j, 1]
+    @inbuonds η₀ = η[i, j, 1]
+
+    λ = η₀ / E₀
+
+    @inbounds σ₁₁′ = λ * (Δt * E * stiffness + σ₁₁[i, j, 1]) / (λ + Δt * (1 - P̃))
+
 end
--- src/SeaIceDynamics/Rheologies/initialize_rheologies.jl
+++ src/SeaIceDynamics/Rheologies/initialize_rheologies.jl
@@ -16,30 +16,3 @@ function mask_immersed_field!(rheology::AbstractRheology)
 
     return nothing
 end
-
-"""
-    initialize_rheology!(model, rheology::AbstractRheology)
-
-Initialize the elasto-visco-plastic rheology.
-In this step we calculate the ice strength given the ice mass (thickness and concentration).
-"""
-function initialize_rheology!(model, rheology::AbstractRheology)
-    h = model.ice_thickness
-    ℵ = model.ice_concentration
-
-    P  = rheology.ice_strength
-    P★ = rheology.ice_compressive_strength
-    C  = rheology.ice_compaction_hardening
-    
-    launch!(architecture(model.grid), model.grid, :xy, _compute_ice_strength!, P, P★, C, h, ℵ)
-
-    fill_halo_regions!(P, model.clock, fields(model))
-    
-    return nothing
-end
-
-# The parameterization for an `ExplicitViscoPlasticRheology`
-@kernel function _compute_ice_strength!(P, P★, C, h, ℵ)
-    i, j = @index(Global, NTuple)
-    @inbounds P[i, j, 1] = @inbounds P★ * h[i, j, 1] * exp(- C * (1 - ℵ[i, j, 1])) 
-end
