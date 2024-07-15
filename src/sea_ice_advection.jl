a269944702edd576cfef79b9764c0a874a70b3da
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-07-15 08:00:29 -0400

    update to nee changes

---
+++ src/sea_ice_advection.jl
@@ -0,0 +1,66 @@
+using Oceananigans.Operators
+using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, conditional_flux_fcc, conditional_flux_cfc
+using Oceananigans.Advection: TracerAdvection, _advective_tracer_flux_x, _advective_tracer_flux_y
+
+# To obtain better numerical properties, the ice thickness is advected together 
+# with the concentration, i.e.:
+# 
+# A = ℵ⁻¹ ∇ ⋅ (uℵh) 
+#
+# instead of the classical 
+# 
+# A = ∇ ⋅ (uh)
+
+_advective_thickness_flux_x(args...) = advective_thickness_flux_x(args...)
+
+_advective_thickness_flux_y(args...) = advective_thickness_flux_y(args...)
+
+_advective_thickness_flux_x(i, j, k, ibg::ImmersedBoundaryGrid, args...) = 
+    conditional_flux_fcc(i, j, k, ibg, zero(ibg), advective_thickness_flux_x(i, j, k, ibg, args...))
+
+_advective_thickness_flux_y(i, j, k, ibg::ImmersedBoundaryGrid, args...) = 
+    conditional_flux_cfc(i, j, k, ibg, zero(ibg), advective_thickness_flux_y(i, j, k, ibg, args...))
+
+@inline function advective_thickness_flux_x(i, j, k, grid, advection, U, ℵ, h)
+    ϕℵ = advective_tracer_flux_x(i, j, k, grid, advection, U, ℵ) / Axᶠᶜᶜ(i, j, k, grid)
+    ϕh = ϕℵ * advective_tracer_flux_x(i, j, k, grid, advection, U, h)
+    @inbounds ϕh = ifelse(U[i, j, k] == 0, zero(grid), ϕh / U[i, j, k])
+    return ϕh
+end
+
+@inline function advective_thickness_flux_y(i, j, k, grid, advection, V, ℵ, h)
+    ϕℵ = advective_tracer_flux_y(i, j, k, grid, advection, V, ℵ) / Ayᶜᶠᶜ(i, j, k, grid)
+    ϕh = ϕℵ * advective_tracer_flux_y(i, j, k, grid, advection, V, h) 
+    @inbounds ϕh = ifelse(V[i, j, k] == 0, zero(grid), ϕh / V[i, j, k])
+    return ϕh
+end
+
+# For thickness, we compute [ℵ⁻¹ ∇ ⋅ (uℵh)]
+@inline function div_Uℵh(i, j, k, grid, advection, U, ℵ, h)
+    ∇Uℵh = 1 / Vᶜᶜᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, _advective_thickness_flux_x, advection, U.u, ℵ, h) +
+                                      δyᵃᶜᵃ(i, j, k, grid, _advective_thickness_flux_y, advection, U.v, ℵ, h))
+
+    @inbounds ℵ⁻¹ = ifelse(ℵ[i, j, k] != 0, 1 / ℵ[i, j, k], zero(grid))
+
+    return ℵ⁻¹ * ∇Uℵh
+end
+
+# For thickness, we compute [ℵ⁻¹ ∇ ⋅ (uℵh)]
+@inline function div_Uℵh(i, j, k, grid, advection::TracerAdvection, U, ℵ, h)
+    ∇Uℵh = 1 / Vᶜᶜᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, _advective_thickness_flux_x, advection.x, U.u, ℵ, h) +
+                                      δyᵃᶜᵃ(i, j, k, grid, _advective_thickness_flux_y, advection.y, U.v, ℵ, h))
+
+    @inbounds ℵ⁻¹ = ifelse(ℵ[i, j, k] != 0, 1 / ℵ[i, j, k], zero(grid))
+
+    return ℵ⁻¹ * ∇Uℵh
+end
+
+@inline horizontal_div_Uc(i, j, k, grid, ::Nothing, U, c) = zero(grid)
+@inline horizontal_div_Uc(i, j, k, grid, advection, U, c) = 
+    1 / Vᶜᶜᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, _advective_tracer_flux_x, advection, U.u, c) +
+                               δyᵃᶜᵃ(i, j, k, grid, _advective_tracer_flux_y, advection, U.v, c))
+                               
+@inline horizontal_div_Uc(i, j, k, grid, advection::TracerAdvection, U, c) = 
+    1 / Vᶜᶜᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, _advective_tracer_flux_x, advection.x, U.u, c) +
+                               δyᵃᶜᵃ(i, j, k, grid, _advective_tracer_flux_y, advection.y, U.v, c))
+                               
---
+++ src/sea_ice_time_stepping.jl
@@ -0,0 +1,169 @@
+using Oceananigans.Architectures: architecture
+using Oceananigans.BoundaryConditions: fill_halo_regions!
+using Oceananigans.TimeSteppers: tick!
+using Oceananigans.Utils: launch!, KernelParameters
+import Oceananigans.ImmersedBoundaries: mask_immersed_field!
+
+using Oceananigans: prognostic_fields
+using KernelAbstractions: @index, @kernel
+using Oceananigans.TimeSteppers: ab2_step_field!
+import Oceananigans.TimeSteppers: time_step!
+
+using ClimaSeaIce.SeaIceDynamics: step_momentum!
+
+mask_immersed_field!(::ConstantField) = nothing
+mask_immersed_field!(::ZeroField)     = nothing
+
+function time_step!(model::SIM, Δt; callbacks=nothing, euler=false)
+    
+    # Be paranoid and update state at iteration 0
+    model.clock.iteration == 0 && update_state!(model, callbacks)
+
+    ab2_timestepper = model.timestepper
+
+    # Change the default χ if necessary, which occurs if:
+    #   * We detect that the time-step size has changed.
+    #   * We detect that this is the "first" time-step, which means we
+    #     need to take an euler step. Note that model.clock.last_Δt is
+    #     initialized as Inf
+    #   * The user has passed euler=true to time_step!
+    euler = euler || (Δt != model.clock.last_Δt)
+    
+    # If euler, then set χ = -0.5
+    minus_point_five = convert(eltype(model.grid), -0.5)
+    χ = ifelse(euler, minus_point_five, ab2_timestepper.χ)
+
+    # Set time-stepper χ (this is used in ab2_step!, but may also be used elsewhere)
+    χ₀ = ab2_timestepper.χ # Save initial value
+    ab2_timestepper.χ = χ
+
+    # Ensure zeroing out all previous tendency fields to avoid errors in
+    # case G⁻ includes NaNs. See https://github.com/CliMA/Oceananigans.jl/issues/2259
+    if euler
+        @debug "Taking a forward Euler step."
+        for field in ab2_timestepper.G⁻
+            !isnothing(field) && fill!(field, 0)
+        end
+    end
+
+    compute_tracer_tendencies!(model; callbacks)
+    ab2_step_tracers!(model, Δt, χ)
+
+    # TODO: This is an implicit (or split-explicit) step to advance momentum!
+    step_momentum!(model, model.ice_dynamics, Δt, χ)
+
+    # Only the tracers are advanced through an AB2 scheme 
+    # (velocities are stepped in the dynamics step)
+    # so only tracers' tendencies are stored
+    store_tendencies!(model)
+
+    tick!(model.clock, Δt)
+    update_state!(model)
+
+    return nothing
+end
+
+function compute_tracer_tendencies!(model::SIM; callbacks = nothing)
+    grid = model.grid
+    arch = architecture(grid)
+   
+    launch!(arch, grid, :xyz,
+            _compute_tracer_tendencies!,
+            model.timestepper.Gⁿ,
+            model.ice_thickness,
+            grid,
+            model.clock,
+            model.velocities,
+            model.advection,
+            model.ice_concentration,
+            model.ice_thermodynamics,
+            model.external_heat_fluxes.top,
+            model.external_heat_fluxes.bottom,
+            nothing, #model.forcing.h,
+            fields(model))
+
+    return nothing
+end
+
+function ab2_step_tracers!(model::SIM, Δt, χ)
+    grid = model.grid
+    arch = architecture(grid)
+
+    h  = model.ice_thickness
+    ℵ  = model.ice_concentration
+    tracers = model.tracers
+
+    Gⁿ = model.timestepper.Gⁿ
+    G⁻ = model.timestepper.G⁻
+
+    launch!(arch, grid, :xyz, _ab2_step_tracers!, h, ℵ, tracers, Gⁿ, G⁻, Δt, χ)
+
+    return nothing
+end
+
+function store_tendencies!(model::SIM) 
+
+    grid = model.grid
+    arch = architecture(grid)
+    Nx, Ny, _ = size(grid)
+
+    Gⁿ = model.timestepper.Gⁿ
+    G⁻ = model.timestepper.G⁻
+    Nt = length(Gⁿ)
+
+    params = KernelParameters((Nx, Ny, Nt), (0, 0, 0))
+    launch!(arch, model.grid, params, _store_all_tendencies!, G⁻, Gⁿ)
+
+    return nothing
+end
+
+# Thickness and concentration are updated using an AB2 scheme
+# We compute hⁿ⁺¹ and ℵⁿ⁺¹ in the same kernel to account for ridging: 
+# if ℵ > 1, we reset the concentration to 1 and adjust the thickness 
+# to conserve the total ice volume in the cell.
+@kernel function _ab2_step_tracers!(h, ℵ, tracers, Gⁿ, G⁻, Δt, χ)
+    i, j, k = @index(Global, NTuple)
+
+    FT = eltype(χ)
+    Δt = convert(FT, Δt)
+    one_point_five = convert(FT, 1.5)
+    oh_point_five  = convert(FT, 0.5)
+
+    Ghⁿ = Gⁿ.h
+    Gℵⁿ = Gⁿ.ℵ
+    
+    Gh⁻ = G⁻.h
+    Gℵ⁻ = G⁻.ℵ
+
+    # Update ice thickness, clipping negative values
+    @inbounds begin
+        h⁺ = h[i, j, k] + Δt * ((one_point_five + χ) * Ghⁿ[i, j, k] - (oh_point_five + χ) * Gh⁻[i, j, k])
+        h⁺ = max(0, h⁺)
+
+        # Belongs in update state?
+        # That's certainly a simple model for ice concentration
+        ℵ⁺ = ℵ[i, j, k] + Δt * ((one_point_five + χ) * Gℵⁿ[i, j, k] - (oh_point_five + χ) * Gℵ⁻[i, j, k])
+        ℵ⁺ = max(0, ℵ⁺)
+        
+        # Ridging! if ℵ > 1, we reset the concentration to 1 and increase the thickness accordingly
+        # to maintain a constant ice volume
+        h[i, j, k] = ifelse(ℵ⁺ > 1, h⁺ * ℵ⁺, h⁺)
+        ℵ[i, j, k] = ifelse(ℵ⁺ > 1, 1, ℵ⁺)
+    end 
+end
+
+@kernel function _store_all_tendencies!(G⁻, Gⁿ) 
+    i, j, n = @index(Global, NTuple)
+    @inbounds G⁻[n][i, j, 1] = Gⁿ[n][i, j, 1]
+end
+
+function update_state!(model::SIM, callbacks = nothing)
+    
+    foreach(prognostic_fields(model)) do field
+        mask_immersed_field!(field)
+    end
+
+    fill_halo_regions!(prognostic_fields(model), model.clock, fields(model))
+
+    return nothing
+end
