ab1683abf8658848f2593172c35f60d6011bb14f
Simone Silvestri <silvestri.simone0@gmail.com>
2024-08-09 08:49:47 -0400

    fix solver

--- src/SeaIceThermodynamics/HeatBoundaryConditions/top_heat_boundary_conditions.jl
+++ src/SeaIceThermodynamics/HeatBoundaryConditions/top_heat_boundary_conditions.jl
@@ -96,5 +96,7 @@ using RootSolvers: SecantMethod, find_zero, CompactSolution
 
     solution = find_zero(flux_balance, method, solution_type)
 
-    return solution.root
+    h = @inbounds model_fields.h[i, j, 1]
+        
+    return ifelse(h > 0, solution.root, Tu)
 end
