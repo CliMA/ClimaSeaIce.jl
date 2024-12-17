84356b4d1c574fb4f8f9b47ee1168e6592f0acbc
simone-silvestri <silvestri.simone0@gmail.com>
2024-12-17 07:43:27 -0500

    add fallback

--- src/SeaIceDynamics/Rheologies/nothing_rheology.jl
+++ src/SeaIceDynamics/Rheologies/nothing_rheology.jl
@@ -1,26 +1,26 @@
 ####
-#### Nothing rheology a.k.a. "Free drift"
+#### Nothing rheology a.k.a. "Free drift" (fallback implementation)
 ####
 
 # This file shows all the functions that need to be extended 
 # when defining a new rheology compatible with the `ExplicitMomentumSolver`
 
 # Fields needed to compute stresses (and stresses themselves)
-required_auxiliary_fields(grid, ::Nothing) = nothing
+required_auxiliary_fields(grid, rheology) = nothing
 
 # All initializations required before the substepping begins
-initialize_rheology!(model, ::Nothing) = nothing
+initialize_rheology!(model, rheology) = nothing
 
 # Computation of the stresses within the substepping (before the momentum substep)
-compute_stresses!(model, solver, ::Nothing, args...) = nothing
+compute_stresses!(model, solver, rheology, args...) = nothing
 
 # Internal stress divergence terms in the velocity tendencies
-@inline ∂ⱼ_σ₁ⱼ(i, j, k, grid, ::Nothing) = zero(grid)
-@inline ∂ⱼ_σ₂ⱼ(i, j, k, grid, ::Nothing) = zero(grid)
+@inline ∂ⱼ_σ₁ⱼ(i, j, k, grid, rheology, fields) = zero(grid)
+@inline ∂ⱼ_σ₂ⱼ(i, j, k, grid, rheology, fields) = zero(grid)
 
 # Additional tendency terms specific to a certain rheology
 @inline rheology_specific_forcing_x(i, j, k, grid, args...) = zero(grid) 
 @inline rheology_specific_forcing_y(i, j, k, grid, args...) = zero(grid)
 
 # Fallback for no specific scheme => α = β = substeps 
-@inline rheology_substeps(i, j, k, grid, ::Nothing, substeps, fields) = substeps 
+@inline rheology_substeps(i, j, k, grid, rheology, substeps, fields) = substeps 
