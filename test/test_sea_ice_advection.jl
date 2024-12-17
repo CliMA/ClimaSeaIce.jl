04dcb3248415606775f808e9ace937a9d463f053
simone-silvestri <silvestri.simone0@gmail.com>
2024-12-17 05:34:25 -0500

    very simple test

---
+++ test/test_sea_ice_advection.jl
@@ -0,0 +1,17 @@
+using ClimaSeaIce
+
+@testset "Sea ice advection" begin
+    @info "Running sea ice advection test"
+
+    grid = RectilinearGrid(size=(10, 10), x=(0, 1), y=(0, 1), topology=(Bounded, Bounded, Flat))
+
+    model = SeaIceModel(grid, advection=WENO()) 
+
+    @test !(model.velocities.u isa Nothing)
+    @test !(model.velocities.v isa Nothing)
+
+    @test begin
+        time_step!(model, 1)
+        true
+    end
+end
