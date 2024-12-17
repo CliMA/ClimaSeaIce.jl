88ece704bd141adba1a1f9ba9c61844d73e99e58
simone-silvestri <silvestri.simone0@gmail.com>
2024-12-17 07:37:14 -0500

    remember the grid!

--- src/SeaIceDynamics/Rheologies/nothing_rheology.jl
+++ src/SeaIceDynamics/Rheologies/nothing_rheology.jl
@@ -6,7 +6,7 @@
 # when defining a new rheology compatible with the `ExplicitMomentumSolver`
 
 # Fields needed to compute stresses (and stresses themselves)
-required_auxiliary_fields(::Nothing) = nothing
+required_auxiliary_fields(grid, ::Nothing) = nothing
 
 # All initializations required before the substepping begins
 initialize_rheology!(model, ::Nothing) = nothing
