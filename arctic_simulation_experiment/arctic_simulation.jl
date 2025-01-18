c2bb4ba67198be3aec3c93424ec11ac2efbf388a
simone-silvestri <silvestri.simone0@gmail.com>
2025-01-18 13:56:37 -0500

    try an arctic simulation

---
+++ arctic_simulation_experiment/arctic_simulation.jl
@@ -0,0 +1,123 @@
+using Oceananigans
+using OrthogonalSphericalShellGrids
+using ClimaOcean
+using ClimaOcean.ECCO
+using ClimaOcean.JRA55
+using ClimaSeaIce
+using ClimaSeaIce.Rheologies
+using ClimaSeaIce.SeaIceMomentumEquations   
+using Oceananigans.Units
+using Printf
+
+include("prescribed_external_stress.jl")
+
+arch = GPU()
+
+sea_ice_grid  = TripolarGrid(arch; size=(680, 220, 1), southernmost_latitude=55, z=(-30, 0), halo=(5, 5, 4))
+bottom_height = regrid_bathymetry(sea_ice_grid, interpolation_passes=1, minimum_depth=0, major_basins=Inf)
+grid = ImmersedBoundaryGrid(sea_ice_grid, GridFittedBottom(bottom_height))
+
+#####
+##### Prescribed ocean velocities from ECCO 
+#####
+
+# dates = ECCO.all_ECCO_dates(ECCO2Daily())[1:10] # 10 days of evolution
+
+# u_velocity = ECCO.ECCOMetadata(:u_velocity; version=ECCO2Daily(), dates)
+# v_velocity = ECCO.ECCOMetadata(:u_velocity; version=ECCO2Daily(), dates)
+
+# uₒ = ECCO.ECCOFieldTimeSeries(u_velocity, arch)
+# vₒ = ECCO.ECCOFieldTimeSeries(v_velocity, arch)
+
+ρₒ = 1025.0 # kg/m³
+Cᴰ = 5.5e-3 # N/m²
+
+τᵤₒ = τᵥₒ = PrescribedOceanStress(ρₒ, Cᴰ)
+
+##### 
+##### Prescribed JRA55 Atmosphere
+#####
+
+uₐ = JRA55.JRA55_field_time_series(:eastward_velocity;  architecture=arch, backend=JRA55NetCDFBackend(10))
+vₐ = JRA55.JRA55_field_time_series(:northward_velocity; architecture=arch, backend=JRA55NetCDFBackend(10))
+
+ρₐ = 1.125  # kg/m³
+Cᴰ = 1.2e-3 # N/m²
+
+τᵤₐ = τᵥₐ = PrescribedAtmosphereStress(uₐ, vₐ, ρₐ, Cᴰ, uₐ.grid)
+
+#####
+##### Set up the model
+#####
+
+u_bcs = FieldBoundaryConditions(top=nothing, bottom=nothing)
+v_bcs = FieldBoundaryConditions(top=nothing, bottom=nothing)
+
+# We use an elasto-visco-plastic rheology and WENO seventh order 
+# for advection of h and ℵ
+momentum_equations = SeaIceMomentumEquation(grid; 
+                                            coriolis = HydrostaticSphericalCoriolis(),
+                                            rheology = ElastoViscoPlasticRheology(),
+                                            solver   = SplitExplicitSolver(substeps=120))
+advection = WENO(; order = 7)
+
+# Define the model!
+model = SeaIceModel(grid; 
+                    top_momentum_stress = (u = τᵤₐ, v = τᵥₐ),
+                    bottom_momentum_stress = (u = τᵤₒ, v = τᵥₒ),
+                    ice_dynamics = momentum_equations,
+                    ice_thermodynamics = nothing, # No thermodynamics here
+                    advection,
+                    timestepper = :QuasiAdamsBashforth2,
+                    boundary_conditions = (u = u_bcs, v = v_bcs))
+
+# We start with thickenss and concentration from climatology
+set!(model.ice_thickness,     ECCOMetadata(:sea_ice_thickness),     inpainting=ClimaOcean.DataWrangling.NearestNeighborInpainting(1))
+set!(model.ice_concentration, ECCOMetadata(:sea_ice_area_fraction), inpainting=ClimaOcean.DataWrangling.NearestNeighborInpainting(1))
+
+simulation = Simulation(model, Δt=120, stop_iteration=1) #stop_time=2days)
+
+# Container to hold the data
+htimeseries = []
+ℵtimeseries = []
+utimeseries = []
+vtimeseries = []
+
+## Callback function to collect the data from the `sim`ulation
+function accumulate_timeseries(sim)
+    h = sim.model.ice_thickness
+    ℵ = sim.model.ice_concentration
+    u = sim.model.velocities.u
+    v = sim.model.velocities.v
+    push!(htimeseries, deepcopy(Array(interior(h))))
+    push!(ℵtimeseries, deepcopy(Array(interior(ℵ))))
+    push!(utimeseries, deepcopy(Array(interior(u))))
+    push!(vtimeseries, deepcopy(Array(interior(v))))
+end
+
+wall_time = [time_ns()]
+
+function progress(sim) 
+    h = sim.model.ice_thickness
+    ℵ = sim.model.ice_concentration
+    u = sim.model.velocities.u
+    v = sim.model.velocities.v
+
+    hmax = maximum(interior(h))
+    ℵmin = minimum(interior(ℵ))
+    umax = maximum(interior(u)), maximum(interior(v))
+    step_time = 1e-9 * (time_ns() - wall_time[1])
+
+    @info @sprintf("Time: %s, Iteration %d, Δt %s, max(vel): (%.2e, %.2e), max(h): %.2f, min(ℵ): %.2f, wtime: %s \n",
+                   prettytime(sim.model.clock.time),
+                   sim.model.clock.iteration,
+                   prettytime(sim.Δt),
+                   umax..., hmax, ℵmin, prettytime(step_time))
+
+     wall_time[1] = time_ns()
+end
+
+simulation.callbacks[:progress] = Callback(progress, IterationInterval(5))
+simulation.callbacks[:save]     = Callback(accumulate_timeseries, IterationInterval(5))
+
+run!(simulation)
---
+++ arctic_simulation_experiment/prescribed_external_stress.jl
@@ -0,0 +1,121 @@
+using ClimaSeaIce
+using ClimaOcean
+using ClimaOcean.ECCO
+using Adapt
+using Oceananigans
+using Oceananigans.Operators: intrinsic_vector
+using Oceananigans.Grids: node
+using Oceananigans.Fields: interpolate, instantiated_location, flattened_unique_values
+using Oceananigans.Models: extract_field_time_series
+using Oceananigans.Operators
+
+using Oceananigans.OutputReaders: update_field_time_series!, extract_field_time_series
+
+# We extend the τx and τy methods to compute the time-dependent stress
+import ClimaSeaIce.SeaIceMomentumEquations: explicit_τx, 
+                                            explicit_τy,
+                                            implicit_τx,
+                                            implicit_τy
+
+import Oceananigans.Fields: fractional_indices
+
+@inline fractional_indices(at_node, grid::ImmersedBoundaryGrid, ℓx, ℓy, ℓz) = fractional_indices(at_node, grid.underlying_grid, ℓx, ℓy, ℓz) 
+
+#####
+##### Defining the types
+#####
+
+struct PrescribedOceanStress{FT}
+    ρₒ :: FT
+    Cᴰ :: FT
+end
+
+Adapt.adapt_structure(to, τ::PrescribedOceanStress) = 
+    PrescribedOceanStress(Adapt.adapt(to, τ.ρₒ),
+                          Adapt.adapt(to, τ.Cᴰ))
+
+@inline explicit_τx(i, j, k, grid, ::PrescribedOceanStress, clock, fields) = zero(grid)
+@inline explicit_τy(i, j, k, grid, ::PrescribedOceanStress, clock, fields) = zero(grid)
+
+@inline function implicit_τx(i, j, k, grid, τ::PrescribedOceanStress, clock, fields) 
+    uᵢ = @inbounds fields.u[i, j, k]
+    vᵢ = ℑxyᶠᶜᵃ(i, j, k, grid, fields.v)
+    
+    return τ.ρₒ * τ.Cᴰ * sqrt(uᵢ^2 + vᵢ^2)
+end
+
+@inline function implicit_τy(i, j, k, grid, τ::PrescribedOceanStress, clock, fields) 
+    uᵢ = ℑxyᶠᶜᵃ(i, j, k, grid, fields.u)
+    vᵢ = @inbounds fields.v[i, j, k]
+    
+    return τ.ρₒ * τ.Cᴰ * sqrt(uᵢ^2 + vᵢ^2)
+end
+
+@inline function interpolate_to_sea_ice_grid(i, j, k, native_grid, fts, grid, time)
+    times = fts.times
+    data = fts.data
+    time_indexing = fts.time_indexing
+    backend = fts.backend
+    loc = instantiated_location(fts)
+    X = node(i, j, k, grid, loc...)
+
+    # Interpolate field time series data onto the current node and time
+    return interpolate(X, time, data, loc, native_grid, times, backend, time_indexing)
+end    
+
+#####
+##### Atmosphere external stress
+#####
+
+struct PrescribedAtmosphereStress{U, V, FT, G}
+    uₐ :: U
+    vₐ :: V
+    ρₐ :: FT
+    Cᴰ :: FT
+    grid :: G
+end
+
+Adapt.adapt_structure(to, τ::PrescribedAtmosphereStress) = 
+     PrescribedAtmosphereStress(Adapt.adapt(to, τ.uₐ), 
+                                Adapt.adapt(to, τ.vₐ), 
+                                Adapt.adapt(to, τ.ρₐ),
+                                Adapt.adapt(to, τ.Cᴰ),
+                                Adapt.adapt(to, τ.grid))
+
+@inline function explicit_τx(i, j, k, grid, τ::PrescribedAtmosphereStress, clock, fields) 
+    time = Time(clock.time)
+    native_grid = τ.grid
+
+    to_loc   = (Face(),   Center(), nothing)
+    from_loc = (Center(), Center(), nothing)
+
+    uₑ, vₑ = interpolate_atmos_velocities(i, j, k, native_grid, to_loc, from_loc, τ.uₐ, τ.vₐ, grid, time)
+    
+    return τ.ρₐ * τ.Cᴰ * sqrt(uₑ^2 + vₑ^2) * uₑ
+end
+
+@inline function explicit_τy(i, j, k, grid, τ::PrescribedAtmosphereStress, clock, fields) 
+    time = Time(clock.time)
+    native_grid = τ.grid
+
+    to_loc   = (Center(), Face(),   nothing)
+    from_loc = (Center(), Center(), nothing)
+
+    uₑ, vₑ = interpolate_atmos_velocities(i, j, k, native_grid, to_loc, from_loc, τ.uₐ, τ.vₐ, grid, time)
+
+    return τ.ρₐ * τ.Cᴰ * sqrt(uₑ^2 + vₑ^2) * vₑ
+end
+
+@inline function interpolate_atmos_velocities(i, j, k, native_grid, to_loc, from_loc, u, v, grid, time)
+    times = u.times
+    u_data = u.data
+    v_data = v.data
+    time_indexing = u.time_indexing
+    backend = u.backend
+    X = node(i, j, k, grid, to_loc...)
+
+    uₑ = interpolate(X, time, u_data, from_loc, native_grid, times, backend, time_indexing)
+    vₑ = interpolate(X, time, v_data, from_loc, native_grid, times, backend, time_indexing)
+
+    return intrinsic_vector(i, j, k, grid, uₑ, vₑ)
+end    
