1dadc9a03fb2bfac4396b6d7c0f256668cef3a6f
Simone Silvestri <silvestri.simone0@gmail.com>
2025-01-29 12:52:07 +0100

    cleanup

--- src/SeaIceMomentumEquations/momentum_tendencies_kernel_functions.jl
+++ src/SeaIceMomentumEquations/momentum_tendencies_kernel_functions.jl
@@ -58,19 +58,5 @@ end
 
      Gⱽ = ifelse(mᵢ ≤ 0, zero(grid), Gⱽ)
 
-   return Gⱽ
+     return Gⱽ
 end
-
-# Fallback
-@inline implicit_τx_coefficient(i, j, k, grid, stress, clock, fields) = zero(grid)
-@inline implicit_τy_coefficient(i, j, k, grid, stress, clock, fields) = zero(grid)
-
-# Fallback
-@inline explicit_τx(i, j, k, grid, stress, clock, fields) = zero(grid)
-@inline explicit_τy(i, j, k, grid, stress, clock, fields) = zero(grid)
-
-@inline explicit_τx(i, j, k, grid, stress::Number, clock, fields) = stress
-@inline explicit_τy(i, j, k, grid, stress::Number, clock, fields) = stress
-
-@inline explicit_τx(i, j, k, grid, stress::AbstractArray, clock, fields) =  @inbounds stress[i, j, k] 
-@inline explicit_τy(i, j, k, grid, stress::AbstractArray, clock, fields) =  @inbounds stress[i, j, k] 
