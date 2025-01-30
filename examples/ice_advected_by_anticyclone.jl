54f9f904bb106f6bd4d2677bb065e9db011fb30c
Simone Silvestri <silvestri.simone0@gmail.com>
2025-01-30 22:01:28 +0100

    change

--- examples/ice_advected_by_anticyclone.jl
+++ examples/ice_advected_by_anticyclone.jl
@@ -142,9 +142,9 @@ momentum_equations = SeaIceMomentumEquation(grid;
                                             bottom_momentum_stress = (u = τᵤₒ, v = τᵥₒ),
                                             coriolis = FPlane(f=1e-4),
                                             ocean_velocities = (u = Uₒ, v = Vₒ),
-                                            minimum_plastic_stress = 1e-10,
                                             rheology = ElastoViscoPlasticRheology(min_substeps=50, 
-                                                                                  max_substeps=500),
+                                                                                  max_substeps=500,
+                                                                                  minimum_plastic_stress=1e-10),
                                             solver   = SplitExplicitSolver(substeps=1000))
 advection = WENO(; order = 7)
 
