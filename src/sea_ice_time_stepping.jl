491bb564547c07eaa8112c44995ddc3b2de03db0
Simone Silvestri <silvestri.simone0@gmail.com>
2025-02-11 11:03:52 +0100

    bugfix for nothing

--- src/sea_ice_time_stepping.jl
+++ src/sea_ice_time_stepping.jl
@@ -100,8 +100,7 @@ end
 function update_model_field_time_series!(model::SeaIceModel, clock::Clock)
     time = Time(clock.time)
 
-    #TODO : change this 
-    possible_fts = (model.tracers, model.external_heat_fluxes, model.dynamics.external_momentum_stresses)
+    possible_fts = (model.tracers, model.external_heat_fluxes, model.dynamics)
     time_series_tuple = extract_field_time_series(possible_fts)
     time_series_tuple = flattened_unique_values(time_series_tuple)
 
