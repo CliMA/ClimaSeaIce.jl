823703e19d5c482d594408fda59565a2e46ad25c
Simone Silvestri <silvestri.simone0@gmail.com>
2025-02-13 18:11:18 +0100

    add this one

--- docs/make.jl
+++ docs/make.jl
@@ -15,6 +15,7 @@ const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 example_scripts = [
     "freezing_bucket.jl",
     "ice_advected_by_anticyclone.jl",
+    "ice_advected_on_coastline.jl",
 ]
 
 for filename in example_scripts
@@ -25,6 +26,7 @@ end
 example_pages = [
     "Freezing bucket" => "literated/freezing_bucket.md",
     "Ice advected by anticyclone" => "literated/ice_advected_by_anticyclone.md",
+    "Ice advected on coastline" => "literated/ice_advected_on_coastline.md",
 ]
 
 #####
