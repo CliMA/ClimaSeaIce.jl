c033058d6f8b67cd46234530ebecf81c515b1847
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-13 13:05:51 +0100

    muuuch better

--- examples/arctic_basin_seasonal_cycle.jl
+++ examples/arctic_basin_seasonal_cycle.jl
@@ -4,8 +4,11 @@
 
 using Oceananigans
 using Oceananigans.Units
-using Oceananigans.Fields: index_binary_search
+using Oceananigans.Utils: Time
 using ClimaSeaIce
+
+# Generate a 0D grid for a single column slab model 
+grid = RectilinearGrid(size=(), topology=(Flat, Flat, Flat))
 
 # Forcings (Semtner 1976, table 1), originally tabulated by Fletcher (1965)
 # Note: these are in kcal, which was deprecated in the ninth General Conference on Weights and
@@ -23,11 +26,9 @@ year_days = month_days * Nmonths
 times_days = 15:30:(year_days - 15)
 times = times_days .* day # times in seconds
 
-albedo = 0.8 # Almost like in Semtner (1976)
-
 # Convert fluxes to the right units
 kcal_to_joules = 4184
-tabulated_shortwave .*= kcal_to_joules / (month_days * days) .* (1 - albedo)
+tabulated_shortwave .*= kcal_to_joules / (month_days * days) 
 tabulated_longwave  .*= kcal_to_joules / (month_days * days)
 tabulated_sensible  .*= kcal_to_joules / (month_days * days)
 tabulated_latent    .*= kcal_to_joules / (month_days * days)
@@ -43,39 +44,43 @@ lines!(ax, times, tabulated_latent)
 
 display(fig)
 
-@inline function linearly_interpolate_flux(i, j, grid, Tₛ, clock, model_fields, parameters)
-    times = parameters.times
-    Q = parameters.flux
-    Nt = length(times)
-    t  = mod(clock.time, 360days)
-    n₁, n₂ = index_binary_search(times, t, Nt)
+# Make them into a FieldTimeSeries for better manipulation
 
-    Q₁ = @inbounds Q[n₁]
-    Q₂ = @inbounds Q[n₂]
+Rs = FieldTimeSeries{Nothing, Nothing, Nothing}(grid, times; time_indexing = Cyclical)
+Rl = FieldTimeSeries{Nothing, Nothing, Nothing}(grid, times; time_indexing = Cyclical)
+Qs = FieldTimeSeries{Nothing, Nothing, Nothing}(grid, times; time_indexing = Cyclical)
+Ql = FieldTimeSeries{Nothing, Nothing, Nothing}(grid, times; time_indexing = Cyclical)
 
-    ñ = if n₁ == n₂
-        n₁
-    else
-        @inbounds (t - times[n₁]) * (n₂ - n₁) / (times[n₂] - times[n₁])
-    end
-
-    return Q₁ * (ñ - n₁) + Q₂ * (n₂ - ñ)
+for (i, time) in enumerate(times)
+    set!(Rs, tabulated_shortwave[i])
+    set!(Rl, tabulated_longwave[i])
+    set!(Qs, tabulated_sensible[i])
+    set!(Ql, tabulated_latent[i])
 end
 
-Q_shortwave = FluxFunction(linearly_interpolate_flux, parameters=(; times, flux=tabulated_shortwave))
-Q_longwave  = FluxFunction(linearly_interpolate_flux, parameters=(; times, flux=tabulated_longwave))
-Q_sensible  = FluxFunction(linearly_interpolate_flux, parameters=(; times, flux=tabulated_sensible))
-Q_latent    = FluxFunction(linearly_interpolate_flux, parameters=(; times, flux=tabulated_latent))
+@inline function linearly_interpolate_flux(i, j, grid, Tₛ, clock, model_fields, flux)
+    t  = Time(clock.time)
+    return flux[i, j, 1, t]
+end
+
+@inline function linearly_interpolate_solar_flux(i, j, grid, Tₛ, clock, model_fields, flux)
+    Q = linearly_interpolate_flux(i, j, grid, Tₛ, clock, model_fields, flux)
+    α = ifelse(Tₛ < -0.1, 0.75, 0.64)
+    return Q * (1 - α)
+end
+
+Q_shortwave = FluxFunction(linearly_interpolate_solar_flux, parameters=Rs)
+Q_longwave  = FluxFunction(linearly_interpolate_flux,       parameters=Rl)
+Q_sensible  = FluxFunction(linearly_interpolate_flux,       parameters=Qs)
+Q_latent    = FluxFunction(linearly_interpolate_flux,       parameters=Ql)
 Q_emission  = RadiativeEmission()
 
-# Generate a 0D grid for a single column slab model 
-grid = RectilinearGrid(size=(), topology=(Flat, Flat, Flat))
-
 top_heat_flux = (Q_shortwave, Q_longwave, Q_sensible, Q_latent, Q_emission)
+
 model = SeaIceModel(grid; top_heat_flux)
 set!(model, h=0.3, ℵ=1) # We start from 300cm of ice and full concentration
 
-simulation = Simulation(model, Δt=8hours, stop_time=4 * 360days)
+simulation = Simulation(model, Δt=1hours, stop_time=2* 360days)
 
 # Accumulate data
 series = []
@@ -84,7 +89,9 @@ function accumulate_timeseries(sim)
     T = model.ice_thermodynamics.top_surface_temperature
     h = model.ice_thickness
     ℵ = model.ice_concentration
-    push!(series, (time(sim), first(h), first(T), first(ℵ)))
+    Qe = model.external_heat_fluxes.top
+    Qe = ClimaSeaIce.SeaIceThermodynamics.HeatBoundaryConditions.getflux(Qe, 1, 1, grid, first(T), model.clock, model.ice_thickness)
+    push!(series, (time(sim), first(h), first(T), first(ℵ), Qe))
 end
 
 simulation.callbacks[:save] = Callback(accumulate_timeseries)
@@ -97,18 +104,21 @@ t = [datum[1] for datum in series]
 h = [datum[2] for datum in series]
 T = [datum[3] for datum in series]
 ℵ = [datum[4] for datum in series]
+Q = [datum[5] for datum in series]
 
 set_theme!(Theme(fontsize=24, linewidth=4))
 
-fig = Figure(size=(1000, 800))
+fig = Figure(size=(1000, 1200))
 
 axT = Axis(fig[1, 1], xlabel="Time (days)", ylabel="Top temperature (ᵒC)")
 axh = Axis(fig[2, 1], xlabel="Time (days)", ylabel="Ice thickness (m)")
 axℵ = Axis(fig[3, 1], xlabel="Time (days)", ylabel="Ice concentration (-)")
+axQ = Axis(fig[4, 1], xlabel="Time (days)", ylabel="Top heat flux (W/m²)")
 
 lines!(axT, t / day, T)
 lines!(axh, t / day, h)
 lines!(axℵ, t / day, ℵ)
+lines!(axQ, t / day, Q)
 
 display(fig)
 
