30f5259cf93cd1673a1fb8fd0f1e6fad8c2ff3ab
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-31 14:25:05 +0200

    cleanup

--- src/SeaIceThermodynamics/slab_heat_and_tracer_fluxes.jl
+++ src/SeaIceThermodynamics/slab_heat_and_tracer_fluxes.jl
@@ -14,7 +14,7 @@ ConductiveFlux(FT::DataType=Float64; conductivity) = ConductiveFlux(convert(FT, 
     Tb = bottom_temperature
     h = ice_thickness
 
-    return ifelse(h <= 0, zero(h), - k * (Tu - Tb) / h)
+    return ifelse(h ≤ 0, zero(h), - k * (Tu - Tb) / h)
 end
 
 @inline function slab_internal_heat_flux(i, j, grid,
