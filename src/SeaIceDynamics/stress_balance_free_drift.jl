8e826febb732878d4fea053e7f23086bf6c7870a
Simone Silvestri <silvestri.simone0@gmail.com>
2025-05-22 11:06:51 +0200

    another bugfix

--- src/SeaIceDynamics/stress_balance_free_drift.jl
+++ src/SeaIceDynamics/stress_balance_free_drift.jl
@@ -15,22 +15,22 @@ Can be used to limit the sea ice velocity when the mass or the concentration are
 as a `dynamics` model itself that substitutes the sea ice momentum equation calculation everywhere.
 """
 struct StressBalanceFreeDrift{T, B} <: AbstractFreeDriftDynamics
-    top_momentum_stess :: T
+    top_momentum_stress :: T
     bottom_momentum_stress :: B
 end
 
 Adapt.adapt_structure(to, s::StressBalanceFreeDrift) = 
     StressBalanceFreeDrift(Adapt.adapt(to, s.top_momentum_stess),
                            Adapt.adapt(to, s.bottom_momentum_stress))
-                           
+
 fields(::StressBalanceFreeDrift) = NamedTuple()
 
 @inline function free_drift_u(i, j, k, grid, f::StressBalanceFreeDrift, clock, fields)
-    τib = implicit_τx_coefficient(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
     τit = implicit_τx_coefficient(i, j, k, grid, f.top_momentum_stress, clock, fields)
+    τib = implicit_τx_coefficient(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
 
-    τeb = explicit_τx(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
     τet = explicit_τx(i, j, k, grid, f.top_momentum_stress, clock, fields)
+    τeb = explicit_τx(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
 
     # Explicit stress
     τe = τeb - τet
@@ -42,11 +42,11 @@ fields(::StressBalanceFreeDrift) = NamedTuple()
 end
 
 @inline function free_drift_v(i, j, k, grid, f::AbstractFreeDriftDynamics, clock, fields) 
-    τib = implicit_τy_coefficient(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
     τit = implicit_τy_coefficient(i, j, k, grid, f.top_momentum_stress, clock, fields)
+    τib = implicit_τy_coefficient(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
 
-    τeb = explicit_τy(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
     τet = explicit_τy(i, j, k, grid, f.top_momentum_stress, clock, fields)
+    τeb = explicit_τy(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
 
     # Explicit stress
     τe = τeb - τet
