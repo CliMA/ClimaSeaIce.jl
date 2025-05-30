ca19ab6ccf40f930468894fcda666f4bf2bb0ed9
Simone Silvestri <silvestri.simone0@gmail.com>
2025-05-30 18:03:04 +0200

    Stress balance free drift model (#72)
    * add this
    
    * Update freezing_of_a_lake.jl
    
    * formatting
    
    * some fixes
    
    * hmm, not sure heat capacity is consistent here
    
    * add to examples?
    
    * the seasonal cycle is not that well predicted I guess
    
    * add this
    
    * the fluxes have wrong signs
    
    * missing a minus and a conversion factor
    
    * some error in the table
    
    * muuuch better
    
    * much better
    
    * fix examples
    
    * add ice salinity
    
    * ok this works
    
    * small fix
    
    * cairomakie
    
    * fix examples
    
    * energy budget
    
    * add an energy budgets
    
    * Update src/SeaIceThermodynamics/slab_thermodynamics_tendencies.jl
    
    Co-authored-by: Gregory L. Wagner <wagner.greg@gmail.com>
    
    * this should work
    
    * add a change to conc
    
    * ifelse
    
    * drop heavy examples
    
    * drop heavy examples
    
    * show figs
    
    * just one dynamical case
    
    * heat budget
    
    * better naming
    
    * correct
    
    * fix the example
    
    * back to 20 days
    
    * add
    
    * flux balance
    
    * no need for C
    
    * add a way to timestep only with  free drift
    
    * add it
    
    * comment
    
    * add it
    
    * fallbacks
    
    * comment
    
    * no need to do this
    
    * inbounds
    
    * last fix
    
    * SeaIcedynamics
    
    * name changes
    
    * fix example
    
    * introducing an adapt
    
    * another bugfix
    
    * an example
    
    * better defaults
    
    * bugfix
    
    * Update stress_balance_free_drift.jl
    
    * Update src/SeaIceDynamics/sea_ice_momentum_equations.jl
    
    * add a quadratic formulation
    
    * add an explanation
    
    * remove one coefficient
    
    * make sure we don't nan
    
    * better comment
    
    * fix another comment
    
    * remove model
    
    * comment
    
    * comment
    
    * add this simplified formula
    
    * bugfix
    
    * leave this case for later?
    
    * add a fallback
    
    * just pass any
    
    * correct locations
    
    * correct default
    
    * adapt bugfix
    
    * Update src/SeaIceDynamics/sea_ice_momentum_equations.jl
    
    * comment
    
    * correct default
    
    * bugfix
    
    * validation
    
    * update Project
    
    ---------
    
    Co-authored-by: Gregory L. Wagner <wagner.greg@gmail.com>
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/SeaIceDynamics/sea_ice_momentum_equations.jl
+++ src/SeaIceDynamics/sea_ice_momentum_equations.jl
@@ -1,12 +1,12 @@
 using ClimaSeaIce.Rheologies
 using Adapt
 
-struct SeaIceMomentumEquation{S, C, R, V, A, ES, FT}
+struct SeaIceMomentumEquation{S, C, R, F, A, ES, FT}
     coriolis :: C
     rheology :: R
     auxiliary_fields :: A
     solver :: S
-    ocean_velocities :: V
+    free_drift :: F
     external_momentum_stresses :: ES
     minimum_concentration :: FT
     minimum_mass :: FT
@@ -17,16 +17,19 @@ struct ExplicitSolver end
 
 """
     SeaIceMomentumEquation(grid; 
-                           coriolis=nothing,
-                           rheology=ElastoViscoPlasticRheology(eltype(grid)),
-                           auxiliary_fields=NamedTuple(),
-                           ocean_velocities=nothing,
-                           solver=ExplicitSolver(),
-                           minimum_concentration=1e-3,
-                           minimum_mass=1.0)
+                           coriolis = nothing,
+                           rheology = ElastoViscoPlasticRheology(eltype(grid)),
+                           auxiliary_fields = NamedTuple(),
+                           top_momentum_stress    = (u=nothing, v=nothing),
+                           bottom_momentum_stress = (u=nothing, v=nothing),
+                           free_drift = StressBalanceFreeDrift(; top_momentum_stress, bottom_momentum_stress),
+                           solver = SplitExplicitSolver(150),
+                           minimum_concentration = 1e-3,
+                           minimum_mass = 1.0)
 
 Constructs a `SeaIceMomentumEquation` object that controls the dynamical evolution of sea-ice momentum.
 The sea-ice momentum obey the following evolution equation:
+
 ```math
     ∂u                   τₒ    τₐ
     -- + f x u = ∇ ⋅ σ + --  + -- 
@@ -46,8 +49,8 @@ Keyword Arguments
 - `coriolis`: Parameters for the background rotation rate of the model.
 - `rheology`: The sea ice rheology model, default is `ElastoViscoPlasticRheology(eltype(grid))`.
 - `auxiliary_fields`: A named tuple of auxiliary fields, default is an empty `NamedTuple()`.
-- `ocean_velocities`: The ocean surface velocities used to limit the sea ice momentum when the mass or the concentration are
-                      below a certain threshold. default is `nothing` (indicating that the free drift velocities are zero).
+- `free_drift`: The free drift velocities used to limit sea ice momentum when the mass or the concentration are
+                below a certain threshold. Default is `nothing` (indicating that the free drift velocities are zero).
 - `solver`: The momentum solver to be used.
 - `minimum_concentration`: The minimum sea ice concentration above which the sea ice velocity is dynamically calculated, default is `1e-3`.
 - `minimum_mass`: The minimum sea ice mass per area above which the sea ice velocity is dynamically calculated, default is `1.0 kg/m²`.
@@ -56,10 +59,10 @@ function SeaIceMomentumEquation(grid;
                                 coriolis = nothing,
                                 rheology = ElastoViscoPlasticRheology(eltype(grid)),
                                 auxiliary_fields = NamedTuple(),
-                                ocean_velocities = nothing,
-                                top_momentum_stress    = (u = nothing, v = nothing),
-                                bottom_momentum_stress = (u = nothing, v = nothing),
-                                solver = ExplicitSolver(),
+                                top_momentum_stress    = nothing,
+                                bottom_momentum_stress = nothing,
+                                free_drift = StressBalanceFreeDrift(; top_momentum_stress, bottom_momentum_stress),
+                                solver = SplitExplicitSolver(150),
                                 minimum_concentration = 1e-3,
                                 minimum_mass = 1.0)
 
@@ -73,18 +76,10 @@ function SeaIceMomentumEquation(grid;
                                   rheology, 
                                   auxiliary_fields, 
                                   solver,
-                                  ocean_velocities,
+                                  free_drift,
                                   external_momentum_stresses,
                                   convert(FT, minimum_concentration),
                                   convert(FT, minimum_mass))
 end
 
 fields(mom::SeaIceMomentumEquation) = mom.auxiliary_fields
-
-# Just passing ocean velocities without mitigation
-@inline free_drift_u(i, j, k, grid, f) = @inbounds f.u[i, j, k] 
-@inline free_drift_v(i, j, k, grid, f) = @inbounds f.v[i, j, k] 
-
-# Passing no velocities
-@inline free_drift_u(i, j, k, grid, ::Nothing) = zero(grid)
-@inline free_drift_v(i, j, k, grid, ::Nothing) = zero(grid)
---
+++ src/SeaIceDynamics/stress_balance_free_drift.jl
@@ -0,0 +1,135 @@
+abstract type AbstractFreeDriftDynamics end
+
+struct StressBalanceFreeDrift{T, B} <: AbstractFreeDriftDynamics
+    top_momentum_stress :: T
+    bottom_momentum_stress :: B
+end
+
+"""
+    StressBalanceFreeDrift{T, B}
+
+A free drift parameterization that computes the free drift velocities as a balance between top and bottom stresses ``τa ≈ τo``.
+
+The only supported configuration is when either the `top_momentum_stess` or the `bottom_momentum_stress` are a 
+`SemiImplicitStress`. The model will compute the free drift velocity exactly assuming that the other stress does 
+not depend on the sea ice velocity. 
+
+Can be used to limit the sea ice velocity when the mass or the concentration are below a certain threshold, or
+as a `dynamics` model itself that substitutes the sea ice momentum equation calculation everywhere.
+"""
+function StressBalanceFreeDrift(; top_momentum_stress = nothing,
+                                  bottom_momentum_stress = nothing)
+
+    if top_momentum_stress isa SemiImplicitStress
+        if bottom_momentum_stress isa SemiImplicitStress
+            throw(ArgumentError("`StressBalanceFreeDrift` supports a `SemiImplicitStress` only for the `top_momentum_stess` or the `bottom_momentum_stress`, not both"))
+        end
+    else
+        if !(bottom_momentum_stress isa SemiImplicitStress)
+            throw(ArgumentError("`StressBalanceFreeDrift` requires using a `SemiImplicitStress` for either the `top_momentum_stess` or the `bottom_momentum_stress`"))
+        end
+    end
+
+    return StressBalanceFreeDrift(top_momentum_stress, bottom_momentum_stress)
+end
+
+Adapt.adapt_structure(to, s::StressBalanceFreeDrift) = 
+    StressBalanceFreeDrift(Adapt.adapt(to, s.top_momentum_stress),
+                           Adapt.adapt(to, s.bottom_momentum_stress))
+
+fields(::StressBalanceFreeDrift) = NamedTuple()
+
+# Stress balance when either the top or the bottom stresses do not depend on ice velocity
+# In this case we have a simplified form of the free drift velocity. 
+# All other formulations are not supported at the moment and would require
+# (1) knowing which stress is velocity-dependent
+# (2) A nonlinear solve in case both stresses are velocity-dependent
+const TISB = StressBalanceFreeDrift{<:Any, <:SemiImplicitStress}
+const BISB = StressBalanceFreeDrift{<:SemiImplicitStress, <:Any}
+
+# Stress balance when only the bottom stress is ice-velocity dependent:
+# Then: 𝒰ᵢ = 𝒰ᴮ - τᵀ / sqrt(Cᴮ * ||τᵀ||)
+@inline function free_drift_u(i, j, k, grid, f::TISB, clock, fields) 
+    τxᵀ = x_momentum_stress(i, j, k, grid, f.top_momentum_stress, clock, fields)
+    τyᵀ = ℑxyᶠᶜᵃ(i, j, k, grid, y_momentum_stress, f.top_momentum_stress, clock, fields)
+    τᵀ  = sqrt(τxᵀ^2 + τyᵀ^2)
+
+    τᴮ = f.bottom_momentum_stress
+    uᴮ = @inbounds τᴮ.uₑ[i, j, k]
+    Cᴮ = τᴮ.ρₑ * τᴮ.Cᴰ
+
+    return uᴮ - ifelse(τᵀ == 0, τᵀ, τxᵀ / sqrt(Cᴮ * τᵀ))
+end
+
+@inline function free_drift_v(i, j, k, grid, f::TISB, clock, fields) 
+    τxᵀ = ℑxyᶜᶠᵃ(i, j, k, grid, x_momentum_stress, f.top_momentum_stress, clock, fields)
+    τyᵀ = y_momentum_stress(i, j, k, grid, f.top_momentum_stress, clock, fields)
+    τᵀ  = sqrt(τxᵀ^2 + τyᵀ^2)
+
+    τᴮ = f.bottom_momentum_stress
+    vᴮ = @inbounds τᴮ.vₑ[i, j, k]
+    Cᴮ = τᴮ.ρₑ * τᴮ.Cᴰ
+
+    return vᴮ - ifelse(τᵀ == 0, τᵀ, τyᵀ / sqrt(Cᴮ * τᵀ))
+end
+
+# Stress balance when only the top stress is ice-velocity dependent:
+# Then: 𝒰ᵢ = 𝒰ᵀ - τᴮ / sqrt(Cᵀ * ||τᴮ||)
+@inline function free_drift_u(i, j, k, grid, f::BISB, clock, fields) 
+    τxᴮ = x_momentum_stress(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
+    τyᴮ = ℑxyᶠᶜᵃ(i, j, k, grid, y_momentum_stress, f.bottom_momentum_stress, clock, fields)
+    τᴮ  = sqrt(τxᴮ^2 + τyᴮ^2)
+
+    τᵀ = f.top_momentum_stess
+    uᵀ = @inbounds τᵀ.uₑ[i, j, k]
+    Cᵀ = τᵀ.ρₑ * τᵀ.Cᴰ
+
+    return uᵀ - ifelse(τᴮ == 0, τᴮ, τxᴮ / sqrt(Cᵀ * τᴮ))
+end
+
+@inline function free_drift_v(i, j, k, grid, f::BISB, clock, fields) 
+    τxᴮ = ℑxyᶜᶠᵃ(i, j, k, grid, x_momentum_stress, f.bottom_momentum_stress, clock, fields)
+    τyᴮ = y_momentum_stress(i, j, k, grid, f.bottom_momentum_stress, clock, fields)
+    τᴮ  = sqrt(τxᴮ^2 + τyᴮ^2)
+
+    τᵀ = f.top_momentum_stess
+    vᵀ = @inbounds τᵀ.vₑ[i, j, k]
+    Cᵀ = τᵀ.ρₑ * τᵀ.Cᴰ
+
+    return vᵀ - ifelse(τᴮ == 0, τᴮ, τyᴮ / sqrt(Cᵀ * τᴮ))
+end
+
+const NoFreeDrift = StressBalanceFreeDrift{<:Nothing, <:Nothing}
+
+@inline free_drift_u(i, j, k, grid, ::NoFreeDrift, clock, fields) = zero(grid)
+@inline free_drift_v(i, j, k, grid, ::NoFreeDrift, clock, fields) = zero(grid)
+
+# Fallbacks for a given velocity field.
+@inline free_drift_u(i, j, k, grid, f::NamedTuple, clock, fields)  = @inbounds f.u[i, j, k] 
+@inline free_drift_v(i, j, k, grid, f::NamedTuple, clock, fields)  = @inbounds f.v[i, j, k] 
+
+# Passing no velocities
+@inline free_drift_u(i, j, k, grid, ::Nothing, clock, fields) = zero(grid)
+@inline free_drift_v(i, j, k, grid, ::Nothing, clock, fields) = zero(grid)
+
+# What if we want to use _only_ the free drift velocities? (not advised)
+function time_step_momentum!(model, dynamics::AbstractFreeDriftDynamics, args...)
+    
+    model_fields = fields(model)
+    clock = model.clock
+    grid  = model.grid
+    arch  = architecture(grid)
+    u, v  = model.velocities
+
+    launch!(arch, grid, :xy, _free_drift_velocity_step!, u, v, grid, dynamics, clock, model_fields)
+
+    return nothing
+end
+
+@kernel function _free_drift_velocity_step!(u, v, grid, dynamics, clock, fields)
+    i, j = @index(Global, NTuple)
+    kᴺ   = size(grid, 3)
+
+    @inbounds u[i, j, 1] = free_drift_u(i, j, kᴺ, grid, dynamics, clock, fields)
+    @inbounds v[i, j, 1] = free_drift_v(i, j, kᴺ, grid, dynamics, clock, fields)
+end
