64082d007688d9e11a7b33771441557df8c1701b
Simone Silvestri <silvestri.simone0@gmail.com>
2025-06-02 10:47:07 +0200

    nothing defaults

--- src/SeaIceDynamics/sea_ice_momentum_equations.jl
+++ src/SeaIceDynamics/sea_ice_momentum_equations.jl
@@ -20,9 +20,9 @@ struct ExplicitSolver end
                            coriolis = nothing,
                            rheology = ElastoViscoPlasticRheology(eltype(grid)),
                            auxiliary_fields = NamedTuple(),
-                           top_momentum_stress    = (u=nothing, v=nothing),
-                           bottom_momentum_stress = (u=nothing, v=nothing),
-                           free_drift = StressBalanceFreeDrift(; top_momentum_stress, bottom_momentum_stress),
+                           top_momentum_stress    = nothing,
+                           bottom_momentum_stress = nothing,
+                           free_drift = nothing,
                            solver = SplitExplicitSolver(150),
                            minimum_concentration = 1e-3,
                            minimum_mass = 1.0)
@@ -61,7 +61,7 @@ function SeaIceMomentumEquation(grid;
                                 auxiliary_fields = NamedTuple(),
                                 top_momentum_stress    = nothing,
                                 bottom_momentum_stress = nothing,
-                                free_drift = StressBalanceFreeDrift(; top_momentum_stress, bottom_momentum_stress),
+                                free_drift = nothing,
                                 solver = SplitExplicitSolver(150),
                                 minimum_concentration = 1e-3,
                                 minimum_mass = 1.0)
