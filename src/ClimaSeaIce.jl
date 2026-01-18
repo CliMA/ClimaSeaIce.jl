4b2722dc279850b1ea1a03055a06c0a8f7812945
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-01-19 04:58:29 +1100

    Reorder imports in ClimaSeaIce.jl

--- src/ClimaSeaIce.jl
+++ src/ClimaSeaIce.jl
@@ -2,25 +2,24 @@
 module ClimaSeaIce
 
 using Oceananigans
-using Oceananigans.Utils
-using Oceananigans.TimeSteppers: Clock
-using Oceananigans.Fields: field, Field, Center, ZeroField, ConstantField
-using Oceananigans.TimeSteppers: tick!, QuasiAdamsBashforth2TimeStepper, RungeKutta3TimeStepper
 using Oceananigans.BoundaryConditions: fill_halo_regions!
+using Oceananigans.Fields: field, Field, Center, ZeroField, ConstantField
 using Oceananigans.Grids: architecture
+using Oceananigans.TimeSteppers: tick!, Clock, QuasiAdamsBashforth2TimeStepper, RungeKutta3TimeStepper
+using Oceananigans.Utils
 
 using KernelAbstractions: @kernel, @index
 
 # Simulations interface
 import Oceananigans: fields, prognostic_fields, prognostic_state, restore_prognostic_state!
+import Oceananigans.Advection: cell_advection_timescale
 import Oceananigans.Fields: set!
+import Oceananigans.ImmersedBoundaries: mask_immersed_field!
 import Oceananigans.Models: AbstractModel
 import Oceananigans.Simulations: reset!, initialize!, iteration
 import Oceananigans.TimeSteppers: time_step!, update_state!
-import Oceananigans.Utils: prettytime
-import Oceananigans.ImmersedBoundaries: mask_immersed_field!
-import Oceananigans.Advection: cell_advection_timescale
 import Oceananigans.TurbulenceClosures: cell_diffusion_timescale
+import Oceananigans.Utils: prettytime
 
 export SeaIceModel, 
        MeltingConstrainedFluxBalance,
