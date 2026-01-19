6b9c496f9dc016b67a6d2c807c0f323704af37d8
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-01-19 14:24:57 +1100

    Bump to Oceananigans v0.104 + adapt checkpointing (#105)
    * bump to Oceananigans v0.104
    
    * import Oceananigans.boundary_conditions
    
    * bump patch release
    
    * add correct checkpointing
    
    * Reorder imports in ClimaSeaIce.jl
    
    * remove stray spaces
    
    * fix checkpointing tests
    
    * Bump version from 0.3.12 to 0.3.13
    
    * Update src/forward_euler_timestepper.jl
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>
    
    * fix docstring formatting
    
    * waiting for 104.1
    
    * drop Oceananigans compat from docs
    
    * include grid in JLD2Writer
    
    * include grid in jld2writer
    
    * revert
    
    ---------
    
    Co-authored-by: Simone Silvestri <silvestri.simone0@gmail.com>

--- src/SeaIceDynamics/sea_ice_momentum_equations.jl
+++ src/SeaIceDynamics/sea_ice_momentum_equations.jl
@@ -1,6 +1,8 @@
 using ClimaSeaIce.Rheologies
 using Adapt
 
+import Oceananigans: prognostic_state, restore_prognostic_state!
+
 struct SeaIceMomentumEquation{S, C, R, F, A, ES, FT}
     coriolis :: C
     rheology :: R
@@ -81,3 +83,18 @@ end
 
 fields(mom::SeaIceMomentumEquation) = mom.auxiliaries.fields
 prognostic_fields(mom::SeaIceMomentumEquation) = prognostic_fields(mom, mom.rheology)
+
+#####
+##### Checkpointing
+#####
+
+function prognostic_state(mom::SeaIceMomentumEquation)
+    pf = prognostic_fields(mom)
+    return prognostic_state(pf)
+end
+
+function restore_prognostic_state!(mom::SeaIceMomentumEquation, state)
+    pf = prognostic_fields(mom)
+    restore_prognostic_state!(pf, state)
+    return mom
+end
