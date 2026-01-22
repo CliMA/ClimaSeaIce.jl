61451d83921321b0ac38577c58c77b29e5495fae
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-22 20:07:01 +0100

    Adding a distributed `JLD2Writer` test (#109)
    * adding a jld2 writer test
    
    * include thermodynamics in tests
    
    * Add newline at end of test_distributed_sea_ice.jl
    
    * Update test_distributed_sea_ice.jl
    
    * Fix tests for length of FieldTimeSeries times
    
    * add method
    
    * include thicnkess and concentration in distributed tests
    
    * this will work
    
    * just make the tests pass
    
    * correct test
    
    ---------
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- test/distributed_tests_utils.jl
+++ test/distributed_tests_utils.jl
@@ -19,16 +19,37 @@ function run_distributed_sea_ice(arch, filename)
 
     u = reconstruct_global_field(model.velocities.u)
     v = reconstruct_global_field(model.velocities.v)
+    h = reconstruct_global_field(model.ice_thickness)
+    ℵ = reconstruct_global_field(model.ice_concentration)
 
     if arch.local_rank == 0
         jldsave(filename; u = Array(interior(u, :, :, 1)),
-                          v = Array(interior(v, :, :, 1)))
+                          v = Array(interior(v, :, :, 1)),
+                          h = Array(interior(h, :, :, 1)),
+                          ℵ = Array(interior(ℵ, :, :, 1)))
     end
 
     MPI.Barrier(MPI.COMM_WORLD)
     MPI.Finalize()
 
     return nothing
+end
+
+# Run the distributed grid simulation and save down reconstructed results
+function run_distributed_sea_ice_jl2dwriter(arch, filename)
+    distributed_grid = RectilinearGrid(arch; 
+                                       size = (100, 100, 1), 
+                                       x = (-10kilometers, 10kilometers), 
+                                       y = (-10kilometers, 10kilometers), 
+                                       z = (-1, 0), 
+                                       halo = (5, 5, 5))
+
+    model = run_distributed_jld2_simulation(distributed_grid, filename)
+
+    MPI.Barrier(MPI.COMM_WORLD)
+    MPI.Finalize()
+
+    return filename
 end
 
 # Just a random simulation on a rectilinear grid
@@ -46,7 +67,7 @@ function run_distributed_simulation(grid)
                                       rheology = ElastoViscoPlasticRheology(),
                                       solver = SplitExplicitSolver(grid, substeps=50))
 
-    model = SeaIceModel(grid; ice_thermodynamics = nothing, dynamics, advection = WENO(order=7))
+    model = SeaIceModel(grid; dynamics, advection = WENO(order=7))
 
     for N in 1:100
         time_step!(model, 1minutes)
@@ -55,3 +76,31 @@ function run_distributed_simulation(grid)
     return model
 end
 
+# Just a random simulation on a rectilinear grid
+function run_distributed_jld2_simulation(grid, filename)
+
+    τᵤ = 0.01
+    τᵥ = 0.01
+    τₒ = SemiImplicitStress()
+
+    # We use an elasto-visco-plastic rheology and WENO seventh order 
+    # for advection of h and ℵ
+    dynamics = SeaIceMomentumEquation(grid; 
+                                      top_momentum_stress = (u=τᵤ, v=τᵥ),
+                                      bottom_momentum_stress = τₒ, 
+                                      rheology = ElastoViscoPlasticRheology(),
+                                      solver = SplitExplicitSolver(grid, substeps=50))
+
+    model = SeaIceModel(grid; dynamics, advection = WENO(order=7))
+    simulation = Simulation(model, Δt = 10, stop_iteration = 20)
+
+    fields = Oceananigans.prognostic_fields(model)
+    simulation.output_writers[:fields] = JLD2Writer(model, fields;
+                                                    schedule = IterationInterval(1),
+                                                    filename,
+                                                    overwrite_existing = true)
+
+    run!(simulation)
+    
+    return model
+end
--- test/test_distributed_sea_ice.jl
+++ test/test_distributed_sea_ice.jl
@@ -30,22 +30,37 @@ run_large_pencil_distributed_grid = """
     run_distributed_sea_ice(arch, "distributed_large_pencil_seaice.jld2")
 """
 
-@testset "Test distributed seaiceGrid simulations..." begin
+run_distributed_jld2writer = """
+    using MPI
+    MPI.Init()
+
+    include("distributed_tests_utils.jl")
+    arch = Distributed(CPU(), partition = Partition(2, 2))
+    run_distributed_sea_ice_jl2dwriter(arch, "distributed_jld2_writer.jld2")
+"""
+
+@testset "Test distributed sea-ice grid simulations..." begin
     # Run the serial computation
     grid = RectilinearGrid(CPU(); 
-                            size = (100, 100, 1), 
-                            x = (-10kilometers, 10kilometers), 
-                            y = (-10kilometers, 10kilometers), 
-                            z = (-1, 0), 
-                            halo = (5, 5, 5))
+                           size = (100, 100, 1), 
+                           x = (-10kilometers, 10kilometers), 
+                           y = (-10kilometers, 10kilometers), 
+                           z = (-1, 0), 
+                           halo = (5, 5, 5))
 
     model = run_distributed_simulation(grid)
 
     # Retrieve Serial quantities
     us, vs = model.velocities
+    hs = model.ice_thickness
+    ℵs = model.ice_concentration
 
     us = interior(us, :, :, 1)
     vs = interior(vs, :, :, 1)
+    hs = interior(hs, :, :, 1)
+    ℵs = interior(ℵs, :, :, 1)
+
+    @info "Testing (4, 1) Distributed sea ice simulations equality"
 
     # Run the distributed grid simulation with a slab configuration
     write("distributed_slab_tests.jl", run_slab_distributed_grid)
@@ -55,12 +70,18 @@ run_large_pencil_distributed_grid = """
     # Retrieve Parallel quantities
     up = jldopen("distributed_yslab_seaice.jld2")["u"]
     vp = jldopen("distributed_yslab_seaice.jld2")["v"]
+    hp = jldopen("distributed_yslab_seaice.jld2")["h"]
+    ℵp = jldopen("distributed_yslab_seaice.jld2")["ℵ"]
 
     rm("distributed_yslab_seaice.jld2")
 
     # Test slab partitioning
     @test all(us .≈ up)
     @test all(vs .≈ vp)
+    @test all(hs .≈ hp)
+    @test all(ℵs .≈ ℵp)
+
+    @info "Testing (2, 2) Distributed sea ice simulations equality"
 
     # Run the distributed grid simulation with a pencil configuration
     write("distributed_tests.jl", run_pencil_distributed_grid)
@@ -70,15 +91,18 @@ run_large_pencil_distributed_grid = """
     # Retrieve Parallel quantities
     up = jldopen("distributed_pencil_seaice.jld2")["u"]
     vp = jldopen("distributed_pencil_seaice.jld2")["v"]
+    hp = jldopen("distributed_pencil_seaice.jld2")["h"]
+    ℵp = jldopen("distributed_pencil_seaice.jld2")["ℵ"]
 
     rm("distributed_pencil_seaice.jld2")
 
     @test all(us .≈ up)
     @test all(vs .≈ vp)
+    @test all(hs .≈ hp)
+    @test all(ℵs .≈ ℵp)
 
-    # We try now with more ranks in the x-direction. This is not a trivial
-    # test as we are now splitting, not only where the singularities are, but
-    # also in the middle of the north fold. This is a more challenging test
+    @info "Testing (4, 2) Distributed sea ice simulations equality"
+
     write("distributed_large_pencil_tests.jl", run_large_pencil_distributed_grid)
     run(`$(mpiexec()) -n 8 $(Base.julia_cmd()) --project -O0 distributed_large_pencil_tests.jl`)
     rm("distributed_large_pencil_tests.jl")
@@ -86,9 +110,38 @@ run_large_pencil_distributed_grid = """
     # Retrieve Parallel quantities
     up = jldopen("distributed_large_pencil_seaice.jld2")["u"]
     vp = jldopen("distributed_large_pencil_seaice.jld2")["v"]
+    hp = jldopen("distributed_large_pencil_seaice.jld2")["h"]
+    ℵp = jldopen("distributed_large_pencil_seaice.jld2")["ℵ"]
 
     rm("distributed_large_pencil_seaice.jld2")
 
     @test all(us .≈ up)
     @test all(vs .≈ vp)
+    @test all(hs .≈ hp)
+    @test all(ℵs .≈ ℵp)
+
+    @info "Testing JLD2Writer on distributed sea ice simulations"
+
+    write("distributed_jld2writer_tests.jl", run_distributed_jld2writer)
+    run(`$(mpiexec()) -n 4 $(Base.julia_cmd()) --project -O0 distributed_jld2writer_tests.jl`)
+    rm("distributed_jld2writer_tests.jl")
+
+    @test isfile("distributed_jld2_writer_rank0.jld2")
+    @test isfile("distributed_jld2_writer_rank1.jld2")
+    @test isfile("distributed_jld2_writer_rank2.jld2")
+    @test isfile("distributed_jld2_writer_rank3.jld2")
+
+    # This does not work, see https://github.com/CliMA/Oceananigans.jl/pull/5188 
+    # h = FieldTimeSeries("distributed_jld2_writer.jld2", "h")
+    # ℵ = FieldTimeSeries("distributed_jld2_writer.jld2", "ℵ")
+    h = FieldTimeSeries("distributed_jld2_writer_rank0.jld2", "h")
+    ℵ = FieldTimeSeries("distributed_jld2_writer_rank0.jld2", "ℵ")
+
+    @test length(h.times) == 21
+    @test length(ℵ.times) == 21
+
+    rm("distributed_jld2_writer_rank0.jld2")
+    rm("distributed_jld2_writer_rank1.jld2")
+    rm("distributed_jld2_writer_rank2.jld2")
+    rm("distributed_jld2_writer_rank3.jld2")
 end
