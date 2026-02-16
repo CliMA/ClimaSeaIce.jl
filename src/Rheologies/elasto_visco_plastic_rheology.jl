e41a85a2f25eb9bc7c406c7cd9752470cfd4d85d
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-02-16 12:03:39 +1100

    minor tweaks

--- src/Rheologies/elasto_visco_plastic_rheology.jl
+++ src/Rheologies/elasto_visco_plastic_rheology.jl
@@ -7,7 +7,7 @@ using Adapt
 using KernelAbstractions: @kernel, @index
 
 ## The equations are solved in an iterative form following the EVP rheology of
-## Kimmritz et al (2016) (https://www.sciencedirect.com/science/article/pii/S1463500317300690)
+## Kimmritz et al. (2016); doi: 10.1016/j.ocemod.2017.05.006
 #
 # Where:
 # σᵢⱼ(u) = 2η ϵ̇ᵢⱼ + [(ζ - η) * (ϵ̇₁₁ + ϵ̇₂₂) - P / 2] δᵢⱼ
@@ -52,22 +52,21 @@ struct IceStrength end
                                pressure_formulation = ReplacementPressure())
 
 Constructs an `ElastoViscoPlasticRheology` object representing a "modified" elasto-visco-plastic
-rheology for slab sea ice dynamics that follows the implementation of Kimmritz et al (2016).
+rheology for slab sea ice dynamics that follows the implementation of Kimmritz et al. (2016).
 The stress tensor is computed following the constitutive relation:
 ```math
-σᵢⱼ = 2η ϵ̇ᵢⱼ + [(ζ - η) * (ϵ̇₁₁ + ϵ̇₂₂) - P / 2] δᵢⱼ
+σᵢⱼ = 2η ϵ̇ᵢⱼ + [(ζ - η) (ϵ̇₁₁ + ϵ̇₂₂) - P / 2] δᵢⱼ
 ```
 where ``ϵ̇ᵢⱼ`` are the strain rates, ``η`` is the shear viscosity, ``ζ`` is the bulk viscosity,
 and ``P`` is the ice strength (acting as the isotropic part of the stress tensor)
-parameterized as ``P_★ h \\exp[ - C ( 1 - ℵ )]`` where ``P_★`` is the `ice_compressive_strength`,
+parameterized as ``P_\\star h \\exp[ - C ( 1 - ℵ )]`` where ``P_\\star`` is the `ice_compressive_strength`,
 ``C`` is the `ice_compaction_hardening`, ``h`` is the ice thickness, and ``ℵ`` is the ice concentration.
 
 The stresses are substepped using a dynamic substepping coefficient ``α`` that is
-spatially varying and computed dynamically as in Kimmritz et al (2016)
-In particular: ``α = \\sqrt{γ²}``
-where ``γ² = ζ c_α (Δt / mᵢ) / A_z`` is a stability parameter with ``A_z`` is the area of the grid cell,
-``mᵢ`` the ice mass, ``Δt`` the time step, and ``c_α`` a numerical stability parameter which controls the
-strength of ``γ²``.
+spatially varying and computed dynamically as done by Kimmritz et al. (2016).
+In particular: ``α = \\sqrt{γ²}``, where ``γ² = ζ c_α (Δt / mᵢ) / A_z`` is a stability parameter
+with ``A_z`` is the area of the grid cell, ``mᵢ`` the ice mass, ``Δt`` the time step, and ``c_α`` a
+numerical stability parameter which controls the strength of ``γ²``.
 
 The stresses are substepped with:
 ```math
@@ -94,8 +93,10 @@ Keyword Arguments
            transitioning the ice from a plastic to a viscous behaviour. Default: `1e-10`.
 - `min_relaxation_parameter`: Minimum value for the relaxation parameter `α`. Default: `30`.
 - `max_relaxation_parameter`: Maximum value for the relaxation parameter `α`. Default: `500`.
-- `relaxation_strength`: parameter controlling the strength of the relaxation parameter. The maximum value is `π²`, see Kimmritz et al (2016). Default: `π² / 2`.
-- `pressure_formulation`: can use `ReplacementPressure` or `IceStrength`. The replacement pressure formulation avoids ice motion in the absence of forcing. Default: `ReplacementPressure`.
+- `relaxation_strength`: parameter controlling the strength of the relaxation parameter. The maximum value is `π²`;
+                         see Kimmritz et al. (2016). Default: `π² / 2`.
+- `pressure_formulation`: can use `ReplacementPressure` or `IceStrength`. The replacement pressure formulation avoids
+                          ice motion in the absence of forcing. Default: `ReplacementPressure`.
 """
 function ElastoViscoPlasticRheology(FT::DataType = Oceananigans.defaults.FloatType;
                                     ice_compressive_strength = 27500,
@@ -134,7 +135,7 @@ function Auxiliaries(r::ElastoViscoPlasticRheology, grid::AbstractGrid)
     uⁿ = Field{Face,   Center, Nothing}(grid)
     vⁿ = Field{Center, Face,   Nothing}(grid)
     P  = Field{Center, Center, Nothing}(grid)
-    α  = Field{Center, Center, Nothing}(grid) # Dynamic substeps a la Kimmritz et al (2016)
+    α  = Field{Center, Center, Nothing}(grid) # Dynamic substeps a la Kimmritz et al. (2016)
     ζ  = Field{Center, Center, Nothing}(grid)
     Δ  = Field{Center, Center, Nothing}(grid)
 
@@ -258,7 +259,7 @@ end
 
 # Compute the visco-plastic stresses for a slab sea ice model.
 # The function updates the internal stress variables `σ₁₁`, `σ₂₂`, and `σ₁₂` in the `rheology` object
-# following the αEVP formulation of Kimmritz et al (2016).
+# following the αEVP formulation of Kimmritz et al. (2016).
 @kernel function _compute_evp_stresses!(fields, grid, rheology, u, v, h, ℵ, ρᵢ, Δt)
     i, j = @index(Global, NTuple)
     kᴺ   = size(grid, 3)
@@ -298,7 +299,7 @@ end
     mᵢᶠᶠᶜ = ℑxyᶠᶠᵃ(i, j, 1, grid, ice_mass, h, ℵ, ρᵢ)
 
     # Update coefficients for substepping using dynamic substepping
-    # with spatially varying coefficients as in Kimmritz et al (2016)
+    # with spatially varying coefficients as done by Kimmritz et al. (2016)
     γ²ᶜᶜᶜ = ζᶜᶜᶜ * cα * Δt / mᵢᶜᶜᶜ / Azᶜᶜᶜ(i, j, 1, grid)
     γ²ᶜᶜᶜ = ifelse(isnan(γ²ᶜᶜᶜ), α⁺^2, γ²ᶜᶜᶜ) # In case both ζᶜᶜᶜ and mᵢᶜᶜᶜ are zero
     γᶜᶜᶜ  = clamp(sqrt(γ²ᶜᶜᶜ), α⁻, α⁺)
