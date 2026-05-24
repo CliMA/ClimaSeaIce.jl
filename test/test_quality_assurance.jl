55e270faa9e26e6e49327e0b9c9ebd9f84af061b
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-05-25 04:56:39 +1000

    test Aqua

--- test/test_quality_assurance.jl
+++ test/test_quality_assurance.jl
@@ -1,6 +1,40 @@
 using Test
 using ClimaSeaIce
+using Aqua: Aqua
 using ExplicitImports: ExplicitImports
+
+function walk_submodules!(result, visited, mod::Module)
+    for name in sort(names(mod; all=true, imported=false))
+        isdefined(mod, name) || continue
+        value = getproperty(mod, name)
+        if value isa Module &&
+            parentmodule(value) === mod &&
+            !(value in visited) &&
+            value !== mod
+
+            push!(visited, value)
+            push!(result, value)
+            walk_submodules!(result, visited, value)
+        end
+    end
+end
+function get_submodules(mod::Module; self=true)
+    result = self ? Module[mod] : Module[]
+    visited = Set{Module}()
+
+    walk_submodules!(result, visited, mod)
+    return result
+end
+
+@testset "Aqua" begin
+    Aqua.test_all(Breeze; piracies=false)
+
+    # `test_piracies` doesn't recurse in inner modules, so we have to test that manually.
+    @testset "No type piracy in $(mod)" for mod in get_submodules(ClimaSeaIce)
+        Aqua.test_piracies(mod)
+    end
+end
+
 
 @testset "Explicit Imports" begin
     @test ExplicitImports.check_no_implicit_imports(ClimaSeaIce) === nothing
