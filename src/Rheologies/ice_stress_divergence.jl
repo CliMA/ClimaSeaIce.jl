416c24219c547958dfae52e114c2b03e7f4ea012
Simone Silvestri <silvestri.simone0@gmail.com>
2026-06-12 18:48:48 +0200

    add a quick test

--- src/Rheologies/ice_stress_divergence.jl
+++ src/Rheologies/ice_stress_divergence.jl
@@ -33,21 +33,21 @@ const f = Face()
 @inline Δx²_qᶜᶜᶜ(i, j, k, grid, q, args...) = Δxᶜᶜᶜ(i, j, k, grid)^2 * q(i, j, k, grid, args...)
 
 # Stress invariants at their native locations
-@inline σI( i, j, k, grid, args...) = _ice_stress_ux(i, j, k, grid, args...) + _ice_stress_vy(i, j, k, grid, args...)
-@inline σII(i, j, k, grid, args...) = _ice_stress_ux(i, j, k, grid, args...) - _ice_stress_vy(i, j, k, grid, args...)
+@inline σδ(i, j, k, grid, args...) = _ice_stress_ux(i, j, k, grid, args...) + _ice_stress_vy(i, j, k, grid, args...)
+@inline σ𝒯(i, j, k, grid, args...) = _ice_stress_ux(i, j, k, grid, args...) - _ice_stress_vy(i, j, k, grid, args...)
 
 @inline function ∂ⱼ_σ₁ⱼ(i, j, k, grid, rheology, clock, fields)
-    δ = Δyᶠᶜᶜ(i, j, k, grid) * δxᶠᵃᵃ(i, j, k, grid, σI, rheology, clock, fields) / 2
-    T = δxᶠᵃᵃ(i, j, k, grid, Δy²_qᶜᶜᶜ, σII, rheology, clock, fields) / Δyᶠᶜᶜ(i, j, k, grid) / 2
+    δ = Δyᶠᶜᶜ(i, j, k, grid) * δxᶠᵃᵃ(i, j, k, grid, σδ, rheology, clock, fields) / 2
+    𝒯 = δxᶠᵃᵃ(i, j, k, grid, Δy²_qᶜᶜᶜ, σ𝒯, rheology, clock, fields) / Δyᶠᶜᶜ(i, j, k, grid) / 2
     S = δyᵃᶜᵃ(i, j, k, grid, Δx²_qᶠᶠᶜ, _ice_stress_uy, rheology, clock, fields) / Δxᶠᶜᶜ(i, j, k, grid)
-    return (δ + T + S) / Azᶠᶜᶜ(i, j, k, grid)
+    return (δ + 𝒯 + S) / Azᶠᶜᶜ(i, j, k, grid)
 end
 
 @inline function ∂ⱼ_σ₂ⱼ(i, j, k, grid, rheology, clock, fields)
-    δ  =   Δxᶜᶠᶜ(i, j, k, grid) * δyᵃᶠᵃ(i, j, k, grid, σI, rheology, clock, fields) / 2
-    T  = - δyᵃᶠᵃ(i, j, k, grid, Δx²_qᶜᶜᶜ, σII, rheology, clock, fields) / Δxᶜᶠᶜ(i, j, k, grid) / 2
+    δ  =   Δxᶜᶠᶜ(i, j, k, grid) * δyᵃᶠᵃ(i, j, k, grid, σδ, rheology, clock, fields) / 2
+    𝒯  = - δyᵃᶠᵃ(i, j, k, grid, Δx²_qᶜᶜᶜ, σ𝒯, rheology, clock, fields) / Δxᶜᶠᶜ(i, j, k, grid) / 2
     S =    δxᶜᵃᵃ(i, j, k, grid, Δy²_qᶠᶠᶜ, _ice_stress_vx, rheology, clock, fields) / Δyᶜᶠᶜ(i, j, k, grid)
-    return (δ + T + S) / Azᶜᶠᶜ(i, j, k, grid)
+    return (δ + 𝒯 + S) / Azᶜᶠᶜ(i, j, k, grid)
 end
 
 #####
